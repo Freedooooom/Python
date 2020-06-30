3b2fa4e0e473cc0be84c9f3de46eb4b6e61d7e74
Freedooom <815703743@qq.com>
2020-06-30 22:42:58 +0800

    "修改错误参数“

--- logv2.0.py
+++ logv2.0.py
@@ -1,8 +1,6 @@
 import re
 from datetime import datetime
 
-logline = '''172.17.0.1 - - [24/May/2020:03:09:02 +0000] "GET /assets/fonts/material-icons.css HTTP/1.1" 200 873 "http://localhost/" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.37" "-"'''
-
 pattern = '''(?P<remoteIP>[\d\.]{7,}) - - \[(?P<datetime>[^\[\]]+)\] "(?P<request>[^"]+)" (?P<status>\d+) (?P<size>\d+) "[^"]+" "(?P<UserAgent>[^"]+)"'''
 regex = re.compile(pattern)
 
@@ -11,7 +9,7 @@ def extract(line):
 #    return matcher.groupdict()
     info = None
     if matcher:
-        info = {k:ops.get(k,lambda x: x)(v) for k,v in matcher.groupdict(logline).items()}
+        info = {k:ops.get(k,lambda x: x)(v) for k,v in matcher.groupdict(line).items()}
     return info
 
 ops = {
@@ -28,7 +26,7 @@ with open(r"C:\Free\Script\TMP\log") as f:
     for line in f:
         wordict = extract(line)
         if wordict:
-            print(wordict['remoteIP'])
+            print(wordict['remoteIP'],wordict['request']['url'])
         else:
             print("Not pattern")
             continue
