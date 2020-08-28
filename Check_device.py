c326f4137dbc9d41f89e024d7185dcc1adcb8cac
Mr_Guo <Mr_Guoooo@163.com>
2020-08-28 17:06:57 +0800

    通过测试

--- Check_device.py
+++ Check_device.py
@@ -1,45 +1,43 @@
 import paramiko
 import time
-import threading
 import getpass
 import socket
-import check
 
-def ssh2(hostname,CMD_List,Port=22,Username='admin',Password='admin_default'):
+
+def ssh2(comm_list:list,hostname='192.168.0.1',User='admin',Passwd='admin_default',Port='22'):
     SSH = paramiko.SSHClient()
     SSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        SSH.connect(hostname,username=Username,password=Password,port=Port)
-    except socket.timeout as err:
-        print(err,'Please check your configurationinfomation')
-    print ('\033[1;31m Successfully connect to {} ! \033[0m'.format(hostname))
-    remote_connection = SSH.invoke_shell()
-    Logfile = open('log_{}.txt'.format(hostname),mode='w+',encoding='utf-8')
-    remote_connection.send('terminal line 0 \n')
-    for COMMAND in CMD_List:
-        print ('\033[1;31m------{}--------\033[0m'.format(COMMAND))
-        print(' ')
-        remote_connection.send(COMMAND)
-        time.sleep(1)
+    SSH.connect(hostname,username=User,password=Passwd,port=Port)
+    remote = SSH.invoke_shell()
+    logfile = open('log_{}.txt'.format(hostname),mode='w+',encoding='utf-8')
+    remote.send('terminal line 0 \n')
+    for comm in comm_list:
+        remote.send(comm)
         while True:
             time.sleep(1)
-            result = remote_connection.recv(65535)
-            Logfile.writelines(result.decode())
-            print(result)
-            if result.decode().endswith('<DPX8000>'):
+            result = remote.recv(65535)
+            logfile.writelines(result.decode('utf-8'))
+            if result.decode('utf-8').endswith('>'):
                 break
         time.sleep(1)
+        print('\033[1;3m command {} executed successfully \033[0m'.format(comm))
     else:
-        remote_connection.send('end \n')
-        remote_connection.send('no terminal line \n')
         SSH.close()
-        Logfile.close()
+        logfile.close()
 
-if __name__ == "__main__":
-    Username = input('Please enter your username  >> ')
-    Password = getpass.getpass('Your Password  >> ')
-    IP_Address = ['x.x.x.x']
-    Command_List = ['show version \n','show device \n','show slot information \n','show ip route \n','show interface status \n','show vlan \n','show ip interface brief \n','show run \n']
-    for IP in IP_Address:
-        check.is_IP(IP)
-        ssh2(IP,CMD_List=Command_List,Port=2211,Password=Password)
+if __name__ == '__main__':
+    comm_list = [
+        'show version \n',
+        'show ip route \n',
+        'show ip interface br \n',
+        'show run \n'
+    ]
+    
+    with open('TMP\IP.txt') as IP_list:
+        for ip in IP_list:
+            try:
+                conn_parser = ip.split()
+                ssh2(comm_list,hostname=conn_parser[0],User=conn_parser[1],Passwd=conn_parser[2])
+            except TimeoutError as err:
+                print('connect {} falied'.format(conn_parser[0]))
+                continue
