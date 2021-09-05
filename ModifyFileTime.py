89c9ce9a07277e5880f6bd3e8240d80be1002f0f
MrGuo <Mr_Guoooo@163.com>
2021-09-05 22:36:50 +0800

    修改文件的访问时间和修改时间

---
+++ ModifyFileTime.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import os
+import time
+
+TimeStamp = time.mktime(time.strptime("2013-10-10 23:40:00","%Y-%m-%d %H:%M:%S"))
+
+def ModifyFile(dirstr:str):
+    dirname = Path(dirstr)
+    for f in dirname.iterdir():
+        if f.is_file():
+            os.utime(f,(TimeStamp,TimeStamp))
+        else:
+            os.utime(f,(TimeStamp,TimeStamp))
+            ModifyFile(dirname/f)
+            
+
+
+
+
+if __name__ == "__main__":
+    ModifyFileDir = input("Your want modify file path >> ")
+    
+    ModifyFile (ModifyFileDir)
